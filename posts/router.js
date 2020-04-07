b55734d603845210d391db96227e90eaf505e29c
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-07 13:35:35 -0600

    Implement POSTing to posts endpoint

--- posts/router.js
+++ posts/router.js
@@ -13,4 +13,29 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  const newPost = req.body;
+  if (newPost.title === undefined || newPost.contents === undefined) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+    return;
+  }
+  db.insert(newPost)
+    .then(({ id }) => {
+      db.findById(id)
+        .then((post) => {
+          res.status(201).json(post[0]);
+        })
+        .catch(() => {
+          res.status(500).json({ error: "Couldn't find new post in database" });
+        });
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: "There was an error while saving the post to the database",
+      });
+    });
+});
+
 module.exports = router;
