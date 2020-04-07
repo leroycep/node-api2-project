2ab361e5453cdd8c9a7ddb255b41a80222f2fd48
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-07 13:25:49 -0600

    Implement getting list of posts

--- posts/router.js
+++ posts/router.js
@@ -4,7 +4,13 @@ const db = require("../data/db.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).json([]);
+  db.find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Unable to retrieve list of posts" });
+    });
 });
 
 module.exports = router;
